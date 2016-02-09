d54f52c65b6e1251dab4461d57cf83e77cdc24f5
Hemal Athulathmudali <hathulathmudali@virtusa.com>
2016-02-09 15:24:51 +0530

    removed moment node module

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 (function main(){
 
-	var mt = require('moment-timezone');
+	//var moment = require('moment');
+	var moment = require('moment-timezone');
+
+	var utcDateTime = moment().utc();
+	console.log('UTC TIME: ' + utcDateTime.format());
+
+	console.log('LA TIME: ' + utcDateTime.tz('America/Los_Angeles').format());
+
+	//console.log('Available time zones strings');
+	//console.log(moment.tz.names());
 
 })();
