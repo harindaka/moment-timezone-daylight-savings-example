22e80b4f75fc9cceb2484fb2b66d02acc61e7d66
Hemal Athulathmudali <hathulathmudali@virtusa.com>
2016-02-09 16:42:19 +0530

    added code to demo converting iso 8601 date string to moment

--- index.js
+++ index.js
@@ -6,10 +6,13 @@
 	var now = new Date();
 	var momentFromDate = moment(now);
 	var utcDateTime = momentFromDate.utc();
+	var dateTimeString = utcDateTime.format();
+	var momentFromString = moment(dateTimeString);
 	
-	console.log('UTC TIME: ' + utcDateTime.format());
+	console.log('UTC TIME: ' + dateTimeString);
 	
-	console.log('LA TIME: ' + utcDateTime.tz('America/Los_Angeles').format());
+	console.log('LA TIME (from moment object): ' + utcDateTime.tz('America/Los_Angeles').format());
+	console.log('LA TIME (from ' + dateTimeString + '): ' + momentFromString.tz('America/Los_Angeles').format());
 
 	//console.log('Available time zones strings');
 	//console.log(moment.tz.names());
